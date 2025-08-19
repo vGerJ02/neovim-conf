5cbe865d0db89f80bb12bd4746abda1b1b650a45
vGerJ02 <gjaimejuanguardia@gmail.com>
2025-08-19 22:32:29 +0200

    feat: add sessions, obisidian, octo and lint

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -31,6 +31,8 @@ return { -- Autoformat
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				python = { "black" },
+				go = { "gofumpt" },
 			},
 		},
 	},
---
+++ lua/plugins/octo.lua
@@ -0,0 +1,17 @@
+local function is_gh_installed()
+	return vim.fn.executable("gh") == 1
+end
+
+return is_gh_installed()
+		and {
+			"pwntester/octo.nvim",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"nvim-telescope/telescope.nvim",
+				-- OR 'ibhagwan/fzf-lua',
+				-- OR 'folke/snacks.nvim',
+				"nvim-tree/nvim-web-devicons",
+			},
+			opts = {},
+		}
+	or nil
